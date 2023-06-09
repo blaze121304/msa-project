cad8bcec8cd350c30b3c47c985ccf957b108b678
rusty <blaze121304@gmail.com>
2023-06-09 17:59:40 +0900

    2023.06.09 rusty 설정 변경

---
+++ src/test/java/com/example/msaeurekaclient/MsaEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msaeurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
